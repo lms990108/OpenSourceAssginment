8c963bc3115b7f6c64dbd9fe22d048c867a5ff88
NakHwa <lms990108@gmail.com>
2023-05-15 16:48:19 +0900

    post_list 생성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from . import views
 from accountapp.views import hello_world
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('', views.post_list, name='post_list'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def hello_world(request):
     return render(request, 'accountapp/helloworld.html')
+
+
+def post_list(request):
+    return render(request, 'accountapp/post_list.html', {})
