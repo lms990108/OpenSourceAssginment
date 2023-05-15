e6d36ec2983253f700d368e9e3cd64efa6f8204f
NakHwa <lms990108@gmail.com>
2023-05-15 18:47:17 +0900

    게시글 상세 페이지가 출력되지 않음. 이유는 모르겠음

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from accountapp.views import hello_world
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world'),
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.utils import timezone
 
 from .models import Post
-
-
-def hello_world(request):
-    return render(request, 'accountapp/helloworld.html')
+from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'accountapp/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'accountapp/post_detail.html', {'post': post})
