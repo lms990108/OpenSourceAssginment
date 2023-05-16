a05ce9decc100f58381c8b7d8e18570cca8ea7c8
NakHwa <lms990108@gmail.com>
2023-05-16 13:03:33 +0900

    게시글 추가 및 수정시, 목록으로 돌아가지 않는 오류. 그외 문제x

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 
 def post_list(request):
@@ -13,3 +14,30 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'accountapp/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'accountapp/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'accountapp/post_edit.html', {'form': form})
